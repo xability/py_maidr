860a16414053160cf30602be576f17ff653b8bfe
Ankita-Khiratkar <aak13@illinois.edu>
2023-03-31 03:27:55 -0500

    a.refactored code b.added json option

--- layer_data.py
+++ layer_data.py
@@ -1,63 +1,100 @@
 import pandas as pd
 
+
 class LayerData:
 
-    def data_plot_count_bar(plot_data):
-        
-        if not plot_data.has_data():
-            return "Plot is Empty!"
-        
-        data_y = []
-        y_vars = plot_data.patches
-        for y in y_vars:
-            data_y.append(y.get_height())
+    def __init__(self, data):
+        self.plot_data = data
 
+    def data_plot_count(self, name, is_json):
+        if not self.plot_data.has_data():
+            return "Plot is Empty!"
+
+        data_y = []
         data_x = []
-        x_vars = plot_data.get_xticklabels()
+
+        x_vars = self.plot_data.get_xticklabels()
         for x in x_vars:
             data_x.append(x.get_text())
 
-        data = {}
-        data['x'] = data_x
-        data['y'] = data_y
+        y_vars = self.plot_data.patches
+        for y in y_vars:
+            data_y.append(y.get_height())
 
-        df = pd.DataFrame(data)
-        return df
-    
+        _data = None
+        if is_json:
+            _data = self.to_json(name, data_x, data_y)
 
-    def data_plot_scatter(plot_data):
-        if not plot_data.has_data():
+        else:
+            _data = self.to_df(data_x, data_y)
+
+        return _data
+
+    def data_plot_bar(self, name, is_json):
+        _data = self.data_plot_count(name, is_json)
+        return _data
+
+    def data_plot_scatter(self, name, is_json):
+        if not self.plot_data.has_data():
             return "Plot is Empty!"
-        
+
         data_y = []
         data_x = []
-        data_sc = plot_data.collections[0].get_offsets()
+        data_sc = self.plot_data.collections[0].get_offsets()
         for points in data_sc:
             data_y.append(points.data[1])
             data_x.append(points.data[0])
 
-        data = {}
-        data['x'] = data_x
-        data['y'] = data_y
-        
-        df = pd.DataFrame(data)
-        return df
-    
+        _data = None
 
-    def data_plot_line(plot_data):
-        if not plot_data.has_data():
+        if is_json:
+            _data = self.to_json(name, data_x, data_y)
+
+        else:
+            _data = self.to_df(data_x, data_y)
+
+        return _data
+
+    def data_plot_line(self, name, is_json):
+        if not self.plot_data.has_data():
             return "Plot is Empty!"
-        
+
         data_y = []
         data_x = []
 
-        print(len(plot_data.lines))
-        for data in plot_data.lines:
+        for data in self.plot_data.lines:
             y = data.get_ydata()
             data_y.append(y)
 
             x = data.get_xdata()
             data_x.append(x)
 
-        df = pd.DataFrame({'x': data_x[0], 'y': data_y[0]})
-        return df
+        _data = None
+
+        if is_json:
+            _data = self.to_json(name, data_x[0], data_y[0])
+
+        else:
+            _data = self.to_df(data_x[0], data_y[0])
+
+        return _data
+
+    def to_json(self, name, x_data, y_data):
+        values = []
+        for i in range(len(x_data)):
+            values.append({'x': x_data[i], 'y': y_data[i]})
+
+        data = {
+            'type': name,
+            'data': {
+                'datasets': [{
+                    'data': values
+                }]
+            }
+        }
+
+        return data
+
+    def to_df(self, x_data, y_data):
+        data = pd.DataFrame({'x': x_data, 'y': y_data})
+        return data
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
+# import pandas as pd
 import seaborn as sns
 from layer_data import LayerData
 import matplotlib.pyplot as plt
@@ -7,35 +7,46 @@ import matplotlib.pyplot as plt
 def count_plot():
     df_cp = sns.load_dataset('titanic')
     plot_data_cp = sns.countplot(x=df_cp["class"])
-    ext_data_count = LayerData.data_plot_count_bar(plot_data_cp)
-    print(ext_data_count)
+    ext_data_count = LayerData(plot_data_cp)
+    print(ext_data_count.data_plot_count('count', True))
+    plot_data_cp.clear()
 
 
 def bar_plot():
     df_bp = sns.load_dataset("penguins")
     plot_data_bp = sns.barplot(data=df_bp, x="island", y="body_mass_g")
-    ext_data_bar = LayerData.data_plot_count_bar(plot_data_bp)
-    print(ext_data_bar)
+    ext_data_bar = LayerData(plot_data_bp)
+    print(ext_data_bar.data_plot_bar('bar', True))
+    plot_data_bp.clear()
 
 
 def scatter_plot():
     df_sc = sns.load_dataset("tips")
     plot_data_sc = sns.scatterplot(data=df_sc, x="total_bill", y="tip")
-    ext_data_scatter = LayerData.data_plot_scatter(plot_data_sc)
-    print(ext_data_scatter)
+    ext_data_scatter = LayerData(plot_data_sc)
+    print(ext_data_scatter.data_plot_scatter('scatter', False))
 
 
 def line_plot():
     flights = sns.load_dataset("flights")
     may_flights = flights.query("month == 'May'")
     plot_data_lp = sns.lineplot(data=may_flights, x="year", y="passengers")
-    ext_data_line = LayerData.data_plot_line(plot_data_lp)
-    print(ext_data_line)
+    ext_data_line = LayerData(plot_data_lp)
+    print(ext_data_line.data_plot_line('line', True))
+
+
+def box_plot():
+    tips = sns.load_dataset("tips")
+    ax = sns.boxplot(x="day", y="total_bill", data=tips)
+    # plt.show()
 
 
 def main():
     print("countplot data::::")
     count_plot()
+
+    print("barplot data::::")
+    bar_plot()
 
     print("scatterplot data::::")
     scatter_plot()
@@ -43,9 +54,9 @@ def main():
     print("lineplot data::::")
     line_plot()
 
-    print("\nbarplot data::::")
-    bar_plot()
+    # print("boxplot data::::")
+    # box_plot()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
