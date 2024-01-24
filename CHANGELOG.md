f928eff5e923a5130b3cfbdb45d93ae9b2174346
JooYoung Seo <jseo1005@illinois.edu>
2024-01-23 21:36:34 -0600

    ci: :wrench: add python-semantic-release dependencies and settings

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,3 @@
-# changelog
+## 0.0.1
 
+* Prepared initial setup for py_maidr binder.
--- maidr/__init__.py
+++ maidr/__init__.py
@@ -1,1 +1,5 @@
+# __version__ will be automatically updated by python-semantic-release
+__version__ = "0.0.1"
+
+# Import everything from the maidr module
 from .maidr import *
