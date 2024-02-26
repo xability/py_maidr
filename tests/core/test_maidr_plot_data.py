f1cd8be2cec417be38f7743dee66cfc42098eb72
SaaiVenkat <greenghost1100@gmail.com>
2024-02-26 00:47:02 -0600

    test: remove non-deterministic assert comment

--- tests/core/test_maidr_plot_data.py
+++ tests/core/test_maidr_plot_data.py
@@ -6,7 +6,3 @@ from maidr.core.maidr_plot_data import MaidrPlotData
 def test_instantiate_abstract_maidr_plot_data(mocker):
     with pytest.raises(TypeError) as e:
         MaidrPlotData(mocker.Mock(), mocker.Mock(), mocker.Mock())  # type: ignore
-    assert (
-        "Can't instantiate abstract class MaidrPlotData with abstract "
-        "method _extract_maidr_data" == str(e.value)
-    )
