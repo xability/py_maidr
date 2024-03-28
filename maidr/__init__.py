d2d12028350deec664614dac462f83d4e362a139
Saairam Venkatesh <46614118+SaaiVenkat@users.noreply.github.com>
2024-03-28 01:21:12 -0500

    feat(scatter): support matplotlib and seaborn library (#30)

--- maidr/__init__.py
+++ maidr/__init__.py
@@ -1,7 +1,7 @@
 # __version__ will be automatically updated by python-semantic-release
 __version__ = "0.0.1"
 
-from .maidr import bar, box, count, heat, hist, line, stacked
+from .maidr import bar, box, count, heat, hist, line, scatter, stacked
 
 __all__ = [
     "bar",
@@ -10,5 +10,6 @@ __all__ = [
     "heat",
     "hist",
     "line",
+    "scatter",
     "stacked",
 ]
--- maidr/core/enum/plot_type.py
+++ maidr/core/enum/plot_type.py
@@ -12,4 +12,5 @@ class PlotType(Enum):
     HEAT = "heat"
     HIST = "hist"
     LINE = "line"
+    SCATTER = "point"
     STACKED = "stacked_bar"
