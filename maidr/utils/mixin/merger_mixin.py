5b9468dc5f99f1fc64b747c8fe0ccc445a14310e
SaaiVenkat <greenghost1100@gmail.com>
2024-04-15 08:13:45 -0500

    docs: add docstring to utils

--- maidr/utils/mixin/merger_mixin.py
+++ maidr/utils/mixin/merger_mixin.py
@@ -1,6 +1,7 @@
 class DictMergerMixin:
     @staticmethod
     def merge_dict(to_dict: dict, from_dict: dict) -> dict:
+        """Recursively merge two dictionaries, updating or adding key-value pairs."""
         for key, value in from_dict.items():
             if key in to_dict:
                 if isinstance(to_dict[key], dict) and isinstance(value, dict):
