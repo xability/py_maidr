1385125670bddee3ffd43d9958178fe2b6135eb0
SaaiVenkat <greenghost1100@gmail.com>
2024-05-13 22:24:48 +0530

    fix: test patch semantic

--- maidr/__init__.py
+++ maidr/__init__.py
@@ -1,5 +1,6 @@
-# __version__ will be automatically updated by python-semantic-release
-__version__ = "0.1.0"
+__version__ = (
+    "0.1.0"  # __version__ will be automatically updated by python-semantic-release
+)
 
 from .core import Maidr
 from .core.enum import PlotType
