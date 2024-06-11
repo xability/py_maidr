3f7fd67e1cb8ead704a9a142ac901bcaa641f654
SaaiVenkat <greenghost1100@gmail.com>
2024-06-10 23:02:27 -0400

    chore: update example for syntaxless api

--- example/heatmap/seaborn/example_sns_heatmap.py
+++ example/heatmap/seaborn/example_sns_heatmap.py
@@ -1,35 +1,16 @@
-import os
-
 import matplotlib.pyplot as plt
 import maidr
 import seaborn as sns
 
 
-def get_filepath(filename: str) -> str:
-    current_file_path = os.path.abspath(__file__)
-    directory = os.path.dirname(current_file_path)
-    return os.path.join(directory, filename)
+# Load an example dataset from seaborn
+glue = sns.load_dataset("glue").pivot(index="Model", columns="Task", values="Score")
 
+# Plot a heatmap
+plt.figure(figsize=(10, 8))
+heatmap = sns.heatmap(glue, annot=True, fill_label="Score")
+plt.title("Heatmap of Model Scores by Task")
 
-def plot():
-    # Load an example dataset from seaborn
-    glue = sns.load_dataset("glue").pivot(index="Model", columns="Task", values="Score")
-
-    # Plot a heatmap
-    plt.figure(figsize=(10, 8))
-    heatmap = sns.heatmap(glue, annot=True)
-    plt.title("Heatmap of Model Scores by Task")
-    plt.show()
-
-    return heatmap
-
-
-def main():
-    heatmap = plot()
-    heat_maidr = maidr.heat(heatmap)
-    heat_maidr.save_html(get_filepath("example_sns_heatmap.html"))
-    heat_maidr.show()
-
-
-if __name__ == "__main__":
-    main()
+# Show the plot
+plt.show()
+maidr.show(heatmap)
--- example/scatter/seaborn/example_sns_scatter.py
+++ example/scatter/seaborn/example_sns_scatter.py
@@ -1,39 +1,21 @@
-import os
-
 import matplotlib.pyplot as plt
 import maidr
 import seaborn as sns
 
 
-def get_filepath(filename: str) -> str:
-    current_file_path = os.path.abspath(__file__)
-    directory = os.path.dirname(current_file_path)
-    return os.path.join(directory, filename)
+# Load the Iris dataset
+iris = sns.load_dataset("iris")
 
+# Create a scatter plot
+scatter_plot = sns.scatterplot(
+    data=iris, x="sepal_length", y="sepal_width", hue="species"
+)
 
-def plot():
-    # Load the Iris dataset
-    iris = sns.load_dataset("iris")
+# Adding title and labels (optional)
+plt.title("Iris Sepal Length vs Sepal Width")
+plt.xlabel("Sepal Length")
+plt.ylabel("Sepal Width")
 
-    # Create a scatter plot
-    scatter_plot = sns.scatterplot(
-        data=iris, x="sepal_length", y="sepal_width", hue="species"
-    )
-
-    # Adding title and labels (optional)
-    plt.title("Iris Sepal Length vs Sepal Width")
-    plt.xlabel("Sepal Length")
-    plt.ylabel("Sepal Width")
-
-    return scatter_plot
-
-
-def main():
-    scatter = plot()
-    scatter_maidr = maidr.scatter(scatter)
-    scatter_maidr.save_html(get_filepath("example_sns_scatter.html"))
-    scatter_maidr.show()
-
-
-if __name__ == "__main__":
-    main()
+# Show the plot
+plt.show()
+maidr.show(scatter_plot)
