aebfd89d90c5d64432425745186b1fe9cceab49d
Saairam Venkatesh <greenghost1100@gmail.com>
2024-06-16 15:40:35 -0500

    feat(boxplot): support horizontal orientation (#52)

--- example/box/matplotlib/example_mpl_box.py
+++ example/box/matplotlib/example_mpl_box.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import maidr
 import numpy as np
 
-
 # Generating random data for three different groups
 data_group1 = np.random.normal(100, 10, 200)
 data_group2 = np.random.normal(90, 20, 200)
@@ -11,23 +10,36 @@ data_group3 = np.random.normal(80, 30, 200)
 # Combine these different datasets into a list
 data_to_plot = [data_group1, data_group2, data_group3]
 
-# Create a figure instance
-fig, ax = plt.subplots()
 
 # Create the boxplot
-bp = ax.boxplot(data_to_plot, patch_artist=True)
+def boxplot(is_vert: bool):
+    plt.figure()
+    bp = plt.boxplot(data_to_plot, patch_artist=True, vert=is_vert)
 
-# Customize the boxplot colors
-colors = ["lightblue", "lightgreen", "tan"]
-for patch, color in zip(bp["boxes"], colors):
-    patch.set_facecolor(color)
+    colors = ["lightblue", "lightgreen", "tan"]
+    for patch, color in zip(bp["boxes"], colors):
+        patch.set_facecolor(color)
 
-# Adding labels and title
-ax.set_xticklabels(["Group 1", "Group 2", "Group 3"])
-ax.set_title("Box Plot Example")
-ax.set_xlabel("Group")
-ax.set_ylabel("Values")
+    if is_vert:
+        plt.xticks(ticks=[1, 2, 3], labels=["Group 1", "Group 2", "Group 3"])
+        plt.title("Vertical Box Plot Example")
+        plt.xlabel("Group")
+        plt.ylabel("Values")
+    else:
+        plt.yticks(ticks=[1, 2, 3], labels=["Group 1", "Group 2", "Group 3"])
+        plt.title("Horizontal Box Plot Example")
+        plt.ylabel("Group")
+        plt.xlabel("Values")
 
-# Show the plot
+    return bp
+
+
+# Create the vertical boxplot
+vert = boxplot(is_vert=True)
 plt.show()
-maidr.show(bp)
+maidr.show(vert)
+
+# Create the vertical boxplot
+horz = boxplot(is_vert=False)
+plt.show()
+maidr.show(horz)
--- example/box/seaborn/example_sns_box.py
+++ example/box/seaborn/example_sns_box.py
@@ -6,16 +6,37 @@ import seaborn as sns
 # Load the Iris dataset
 iris = sns.load_dataset("iris")
 
-# Start a new figure
-plt.figure()
 
-# Create box plot
-box_plot = sns.boxplot(x="species", y="petal_length", data=iris)
+def boxplot(orientation: str, fig_num: int):
+    # Start a new figure
+    plt.figure(num=fig_num)
 
-# Adding labels and title
-plt.title("Box Plot of Petal Length by Iris Species")
-plt.xlabel("Species")
-plt.ylabel("Petal Length (cm)")
+    # Create box plot
+    if orientation == "v":
+        box_plot = sns.boxplot(
+            x="species", y="petal_length", data=iris, orient=orientation
+        )
+        plt.xlabel("Species")
+        plt.ylabel("Petal Length (cm)")
+    else:
+        box_plot = sns.boxplot(
+            x="petal_length", y="species", data=iris, orient=orientation
+        )
+        plt.ylabel("Species")
+        plt.xlabel("Petal Length (cm)")
 
+    # Adding labels and title
+    plt.title("Box Plot of Petal Length by Iris Species")
+
+    return box_plot
+
+
+# Create the vertical boxplot
+vert = boxplot(orientation="v", fig_num=1)
 plt.show()
-maidr.show(box_plot)
+maidr.show(vert)
+
+# Create the vertical boxplot
+horz = boxplot(orientation="h", fig_num=2)
+plt.show()
+maidr.show(horz)
