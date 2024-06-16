c5080acd0c08fa0e013388383f2378f4748cf550
SaaiVenkat <greenghost1100@gmail.com>
2024-06-16 11:47:27 -0400

    chore: fix formatting

--- example/bar/seaborn/example_sns_bar_plot.py
+++ example/bar/seaborn/example_sns_bar_plot.py
@@ -9,11 +9,7 @@ penguins = sns.load_dataset("penguins")
 # Create a bar plot showing the average body mass of penguins by species
 plt.figure(figsize=(10, 6))
 b_plot = sns.barplot(
-    x="species",
-    y="body_mass_g",
-    data=penguins,
-    errorbar="sd",
-    palette="Blues_d"
+    x="species", y="body_mass_g", data=penguins, errorbar="sd", palette="Blues_d"
 )
 plt.title("Average Body Mass of Penguins by Species")
 plt.xlabel("Species")
