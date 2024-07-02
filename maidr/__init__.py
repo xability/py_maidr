c32fdfd32473dd354d292d33a19610a4c0a2eb63
Saairam Venkatesh <greenghost1100@gmail.com>
2024-07-02 07:40:22 -0400

    fix: seaborn multi plots in same session (#58)

--- maidr/__init__.py
+++ maidr/__init__.py
@@ -2,10 +2,11 @@ __version__ = "0.4.1"
 
 from .core import Maidr
 from .core.enum import PlotType
-from .patch import barplot, boxplot, heatmap, histogram, lineplot, scatterplot
-from .maidr import save_html, show, stacked
+from .patch import barplot, boxplot, clear, heatmap, histogram, lineplot, scatterplot
+from .maidr import close, save_html, show, stacked
 
 __all__ = [
+    "close",
     "save_html",
     "show",
     "stacked",
--- maidr/maidr.py
+++ maidr/maidr.py
@@ -29,24 +29,6 @@ def stacked(plot: Axes | BarContainer) -> Maidr:
     return FigureManager.create_maidr(ax, PlotType.STACKED)
 
 
-def close() -> None:
-    pass
-
-
-def test_enum_comparison():
-    print(
-        f"PlotType.COUNT == PlotType.BAR: {PlotType.COUNT == PlotType.BAR}"
-    )  # Should be False
-    print(
-        f"PlotType.COUNT is PlotType.BAR: {PlotType.COUNT is PlotType.BAR}"
-    )  # Should be False
-    print(
-        f"PlotType.COUNT == PlotType.COUNT: {PlotType.COUNT == PlotType.COUNT}"
-    )  # Should be True
-    print(
-        f"PlotType.BAR == PlotType.BAR: {PlotType.BAR == PlotType.BAR}"
-    )  # Should be True
-    print(
-        f"PlotType.COUNT is PlotType.COUNT: {PlotType.COUNT is PlotType.COUNT}"
-    )  # Should be True
-    print(f"PlotType.BAR is PlotType.BAR: {PlotType.BAR is PlotType.BAR}")
+def close(plot: Any) -> None:
+    ax = FigureManager.get_axes(plot)
+    FigureManager.destroy(ax.get_figure())
